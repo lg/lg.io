a93eff5dd4cc57ec93fbe0628b5fbd4dc73f2eb7
Larry Gadea <trivex@gmail.com>
2016-01-01 21:21:56 -0500

    removed some debugging

--- assets/up.sh
+++ assets/up.sh
@@ -3,6 +3,5 @@
 DEFAULT_INTERFACE=`/sbin/route get 0.0.0.0 2>/dev/null | awk '/interface: / {print $2}'`
 TAP_INTERFACE=$1
 
-echo $DEFAULT_INTERFACE
 bash -c "tshark -i $DEFAULT_INTERFACE -T fields -e data -l 'udp and dst port 27036' | script -q /dev/null xxd -r -p | nc -b $1 -u 10.8.0.1 27036 > /dev/null" &
 echo $! > /var/run/openvpnup.pid
